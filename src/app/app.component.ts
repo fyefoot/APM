7108862b7e6a4cf41d028531b755bfe2f15f56f7
fyefoot <92579621+fyefoot@users.noreply.github.com>
2022-04-12 13:13:24 +0100

    further updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { CountriesService } from './countries.service';
+import { CountriesService } from './services/countries.service';
 
 @Component({
   selector: 'pm-root',
--- src/app/regions/regions.component.ts
+++ src/app/regions/regions.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CountriesService } from '../countries.service';
+import { CountriesService } from '../services/countries.service';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { AppState } from '../store/models/app.state';
